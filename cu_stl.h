0de28b4ebf58000c72c4f6ddb2a26182bff212cb
zz <zhuguang@ejoy.com>
2017-10-23 15:53:18 +0800

    [ADDED] deque

--- cu_stl.h
+++ cu_stl.h
@@ -8,24 +8,27 @@
 #else
 #include <string>
 #include <vector>
+#include <deque>
 #include <list>
 #include <set>
 #include <map>
 #endif // USE_MM_ALLOCATOR
 
 #ifdef USE_MM_ALLOCATOR
-	#define CU_STR  mm::AllocString
-	#define CU_VEC  mm::AllocVector
-	#define CU_LIST mm::AllocList
-	#define CU_SET  mm::AllocSet
-	#define CU_MAP  mm::AllocMap
+	#define CU_STR   mm::AllocString
+	#define CU_VEC   mm::AllocVector
+	#define CU_DEQUE mm::AllocDeque
+	#define CU_LIST  mm::AllocList
+	#define CU_SET   mm::AllocSet
+	#define CU_MAP   mm::AllocMap
 	#define CU_UNORDERED_MAP mm::AllocUnorderedMap
 #else
-	#define CU_STR  std::string
-	#define CU_VEC  std::vector
-	#define CU_LIST std::list
-	#define CU_SET  std::set
-	#define CU_MAP  std::map
+	#define CU_STR   std::string
+	#define CU_VEC   std::vector
+	#define CU_DEQUE std::deque
+	#define CU_LIST  std::list
+	#define CU_SET   std::set
+	#define CU_MAP   std::map
 	#define CU_UNORDERED_MAP std::unordered_map
 #endif // USE_MM_ALLOCATOR
 
